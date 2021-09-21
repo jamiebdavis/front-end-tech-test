d551ea4eeabb92cef791663793cd99fe69d06886
Jamie Davis <jamied1985@gmail.com>
2021-09-21 14:47:03 +0100

    Removed markup from context
    context should not contain markup

--- src/components/Search/SearchProvider.js
+++ src/components/Search/SearchProvider.js
@@ -5,32 +5,32 @@ import axios from "axios";
 export const SearchContext = createContext();
 
 export default function SearchProvider({ children }) {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [results, setResults] = useState([]);
-    const [numberOfResults] = useState(6);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [numberOfResults] = useState(6);
 
-    const fetchResults = async () => {
-        try {
-            const res = await axios.get(
-                `https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${numberOfResults}&solrTerm=${searchTerm}`
-            );
-            const data = await res.data.results.docs;
-            setResults(data);
-        } catch (error) {
-            console.log("error", error);
-        }
-    };
+  const fetchResults = async () => {
+    try {
+      const res = await axios.get(
+        `https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${numberOfResults}&solrTerm=${searchTerm}`
+      );
+      const data = await res.data.results.docs;
+      setResults(data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
-    useEffect(() => {
-        if (searchTerm.length < 2) return;
-        fetchResults();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [searchTerm]);
+  useEffect(() => {
+    if (searchTerm.length < 2) return;
+    fetchResults();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchTerm]);
 
-    const contextValue = { results, searchTerm, setSearchTerm };
-    return (
-        <div>
-            <SearchContext.Provider value={contextValue}>{children}</SearchContext.Provider>
-        </div>
-    );
+  const contextValue = { results, searchTerm, setSearchTerm };
+  return (
+    <SearchContext.Provider value={contextValue}>
+      {children}
+    </SearchContext.Provider>
+  );
 }
