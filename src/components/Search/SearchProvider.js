5a152863ac678473ca7f425b50ae7750fecac1aa
Jamie Davis <jamie.davis@ao.com>
2021-09-27 14:11:43 +0100

    added useDebounce hook

--- src/components/Search/SearchProvider.js
+++ src/components/Search/SearchProvider.js
@@ -5,32 +5,68 @@ import axios from "axios";
 export const SearchContext = createContext();
 
 export default function SearchProvider({ children }) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [numberOfResults] = useState(6);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [results, setResults] = useState([]);
+    const [numberOfResults] = useState(6);
 
-  const fetchResults = async () => {
+    const [formState, setFormState] = useState(null);
+
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    useEffect(
+        () => {
+            if (searchTerm.length < 2) return;
+
+            if (debouncedSearchTerm) {
+                setFormState("loading");
+                fetchResults(debouncedSearchTerm, numberOfResults).then(results => {
+                    setFormState(null);
+                    setResults(results);
+                });
+            } else {
+                setResults([]);
+                setFormState(null);
+            }
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [debouncedSearchTerm] // Only call effect if debounced search term changes
+    );
+
+    const contextValue = { results, searchTerm, setSearchTerm, formState };
+    return <SearchContext.Provider value={contextValue}>{children}</SearchContext.Provider>;
+}
+
+// API search function
+const fetchResults = async (search, count = 6) => {
     try {
-      const res = await axios.get(
-        `https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${numberOfResults}&solrTerm=${searchTerm}`
-      );
-      const data = await res.data.results.docs;
-      setResults(data);
+        const res = await axios.get(
+            `https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${count}&solrTerm=${search}`
+        );
+        const data = await res.data.results.docs;
+        return data;
     } catch (error) {
-      console.log("error", error);
+        console.log("error", error);
+        return [];
     }
-  };
+};
 
-  useEffect(() => {
-    if (searchTerm.length < 2) return;
-    fetchResults();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchTerm]);
-
-  const contextValue = { results, searchTerm, setSearchTerm };
-  return (
-    <SearchContext.Provider value={contextValue}>
-      {children}
-    </SearchContext.Provider>
-  );
+// Hook
+function useDebounce(value, delay) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(
+        () => {
+            // Update debounced value after delay
+            const handler = setTimeout(() => {
+                setDebouncedValue(value);
+            }, delay);
+            // Cancel the timeout if value changes (also on delay change or unmount)
+            // This is how we prevent debounced value from updating if value is changed ...
+            // .. within the delay period. Timeout gets cleared and restarted.
+            return () => {
+                clearTimeout(handler);
+            };
+        },
+        [value, delay] // Only re-call effect if value or delay changes
+    );
+    return debouncedValue;
 }
